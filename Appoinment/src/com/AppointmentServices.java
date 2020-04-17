1a06a10e26d2c707c4f6b18a7c0f4d02ecb5ad31
IT18007220 <it18007220@my.sliit.lk>
2020-04-17 10:07:59 -0700

    changing DB connection

--- Appoinment/src/com/AppointmentServices.java
+++ Appoinment/src/com/AppointmentServices.java
@@ -34,29 +34,18 @@ public class AppointmentServices {
 	@GET
 	@Path("/")
 	@Produces({ MediaType.TEXT_HTML })
-	public String GetallApp() {
+	public String GetAppointment() {
 		return appObj.GetAllAppoinments();
 	}
 
-//	@GET
-//	@Path("/j")
-//	@Produces({ MediaType.TEXT_HTML })
-//	@Consumes(MediaType.APPLICATION_JSON)
-//	public String GetStudent (String id) {
-//		
-//		JsonObject itemObject = new JsonParser().parse(id).getAsJsonObject();
-//		String appointmentID = itemObject.get("appointmentID").getAsString();
-//		return appObj.GetAppoinments(appointmentID);
-//	}
-	
 	@POST
 	@Path("/")
 	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
 	@Produces(MediaType.TEXT_PLAIN)
 	public String insertAppointment(@FormParam("date") String date, @FormParam("time") String time,
-			@FormParam("patientID") int patientID,
-			@FormParam("doctorID") int doctorID, @FormParam("paymentID") int paymentID) {
-		String output = appObj.insertAppoinment(date, time,patientID, doctorID, paymentID);
+			@FormParam("patientID") int patientID, @FormParam("doctorID") int doctorID,
+			@FormParam("paymentID") int paymentID) {
+		String output = appObj.insertAppoinment(date, time, patientID, doctorID, paymentID);
 		return output;
 	}
 
@@ -70,28 +59,28 @@ public class AppointmentServices {
 		String appointmentID = itemObject.get("appointmentID").getAsString();
 		String date = itemObject.get("date").getAsString();
 		String time = itemObject.get("time").getAsString();
-		//String hospitalID = itemObject.get("hospitalID").getAsString();
 		String patientID = itemObject.get("patientID").getAsString();
 		String doctorID = itemObject.get("doctorID").getAsString();
 		String paymentID = itemObject.get("paymentID").getAsString();
 		String appointmentStatus = itemObject.get("appointmentStatus").getAsString();
-		//String refundID = itemObject.get("refundID").getAsString();
-		
-		String output = appObj.updateAppoinment(appointmentID, date, time, patientID,doctorID,paymentID,appointmentStatus);
-		return output; 
+
+		String output = appObj.updateAppoinment(appointmentID, date, time, patientID, doctorID, paymentID,
+				appointmentStatus);
+		return output;
 	}
-	
+
 	@DELETE
 	@Path("/")
 	@Consumes(MediaType.APPLICATION_XML)
 	@Produces(MediaType.TEXT_PLAIN)
-	public String DelAppoinment(String ApplicationID) {
-		
-		Document doc= Jsoup.parse(ApplicationID, "", Parser.xmlParser());
-		
-		String AppId = doc.select("AID").text();	
-		String output = appObj.DeleteAppoinment(AppId); 
+	public String DeleteAppoinment(String ApplicationID) {
+
+		Document doc = Jsoup.parse(ApplicationID, "", Parser.xmlParser());
+
+		String AppId = doc.select("AID").text();
+		String output = appObj.DeleteAppoinment(AppId);
 		return output;
 	}
 
 }
+
